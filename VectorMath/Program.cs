ba568874348bba88f1046c75cfb912fc80bb5a36
Brenden Levi <brendenlevi@ksu.edu>
2019-12-09 16:11:19 -0600

    12/9 Classwork

--- VectorMath/Program.cs
+++ VectorMath/Program.cs
@@ -7,6 +7,18 @@ namespace VectorMath
         static void Main(string[] args)
         {
             Console.WriteLine("Fun with vectors");
+            Vector3 one = new Vector3(3, 4, 5);
+            Vector3 two = new Vector3(4, 8, 0);
+
+            Console.WriteLine($"Static method Add: {Vector3.Add(one, two)}");
+            Console.WriteLine($"Object method Add: {one.Add(two)}");
+            Console.WriteLine($"Operator Add: {one + two}");
+            Console.WriteLine($"Operator Subtraction: {one - two}");
+            Console.WriteLine($"Scalar multiplication: {2 * one}");
+            Console.WriteLine($"Should not be equal: {one != two}");
+            Console.WriteLine($"Should be equal: {one == new Vector3(3, 4, 5)}");
+            Console.WriteLine($"Should increment: {one++}");
+            Console.WriteLine($"Magnitude: {~one}");
         }
     }
 }
